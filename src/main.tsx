55d855bbe01ed0f73cd7d27e6bda10be96ffca85
konstantine811 <galystyan777@gmail.com>
2025-05-11 22:56:18 +0300

    feat: Add @vercel/analytics for tracking and create vercel.json for rewrites

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import "./i18n"; // <- додали
+import { inject } from "@vercel/analytics";
+
+inject(); // 👈 Це підключає трекінг
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
