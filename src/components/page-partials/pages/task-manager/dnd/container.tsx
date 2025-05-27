d370f15556f30f168f1e97d9ce51f569a6c0d75f
konstantine811 <galystyan777@gmail.com>
2025-05-27 12:42:13 +0300

    feat: add draggable item component with sound effects and task editing capabilities

---
+++ src/components/page-partials/pages/task-manager/dnd/container.tsx
@@ -0,0 +1,131 @@
+import React, { forwardRef, useState } from "react";
+
+import InputCombobox from "@/components/ui-abc/inputs/input-combobox";
+import { Button } from "@/components/ui/button";
+import { GripVertical, Pen, PenOff, X } from "lucide-react";
+import SoundHoverElement from "@/components/ui-abc/sound-hover-element";
+import { SoundTypeElement } from "@/types/sound";
+
+export interface Props {
+  children: React.ReactNode;
+  columns?: number;
+  label?: string;
+  style?: React.CSSProperties;
+  horizontal?: boolean;
+  handleProps?: React.HTMLAttributes<HTMLButtonElement | HTMLDivElement>;
+  scrollable?: boolean;
+  shadow?: boolean;
+  placeholder?: boolean;
+  onClick?(): void;
+  onRemove?(): void;
+  onValueChange?: (value: string) => void;
+}
+
+export const Container = forwardRef<HTMLDivElement, Props>(
+  (
+    {
+      children,
+      columns = 1,
+      handleProps,
+      onRemove,
+      label,
+      placeholder,
+      style,
+      onValueChange,
+      ...props
+    }: Props,
+    ref
+  ) => {
+    const Component = "div";
+    const [isEdit, setIsEdit] = useState(false);
+    return (
+      <Component
+        {...props}
+        ref={ref}
+        style={
+          {
+            ...style,
+            "--columns": columns,
+          } as React.CSSProperties
+        }
+        className="relative"
+      >
+        {label ? (
+          <div className="flex items-center w-full gap-1 relative z-20">
+            <div className="flex w-full gap-2 items-center justify-between">
+              {isEdit ? (
+                <InputCombobox
+                  onValueChange={(value: string) => {
+                    if (onValueChange) onValueChange(value);
+                    setIsEdit(false);
+                  }}
+                  outerValue={label}
+                />
+              ) : (
+                <label
+                  onDoubleClick={() => setIsEdit(true)}
+                  className="text-foreground text-xl ml-3"
+                >
+                  {label}
+                </label>
+              )}
+
+              <SoundHoverElement
+                animValue={0.9}
+                hoverTypeElement={SoundTypeElement.SELECT}
+              >
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => setIsEdit(!isEdit)}
+                >
+                  {isEdit ? <PenOff /> : <Pen />}
+                </Button>
+              </SoundHoverElement>
+            </div>
+
+            {!isEdit && (
+              <div className="flex items-center gap-1">
+                {onRemove && (
+                  <SoundHoverElement
+                    animValue={0.9}
+                    hoverTypeElement={SoundTypeElement.SELECT_2}
+                  >
+                    <Button
+                      variant="destructive"
+                      className="bg-transparent hover:bg-transparent hover:text-destructive text-foreground"
+                      size="icon"
+                      onClick={onRemove}
+                    >
+                      <X />
+                    </Button>
+                  </SoundHoverElement>
+                )}
+                <SoundHoverElement
+                  animValue={0.9}
+                  hoverTypeElement={SoundTypeElement.SHIFT}
+                >
+                  <Button
+                    {...handleProps}
+                    variant="ghost"
+                    size="icon"
+                    className="cursor-move hover:bg-background hover:text-foreground"
+                  >
+                    <GripVertical />
+                  </Button>
+                </SoundHoverElement>
+              </div>
+            )}
+          </div>
+        ) : null}
+        {placeholder ? children : <ul>{children}</ul>}
+        {isEdit && (
+          <div
+            onClick={() => setIsEdit(false)}
+            className="fixed top-0 left-0 w-full h-full z-[10] bg-card/90"
+          ></div>
+        )}
+      </Component>
+    );
+  }
+);
---
+++ src/components/page-partials/pages/task-manager/dnd/item.tsx
@@ -0,0 +1,147 @@
+import React, { useEffect } from "react";
+import type {
+  DraggableSyntheticListeners,
+  UniqueIdentifier,
+} from "@dnd-kit/core";
+import type { Transform } from "@dnd-kit/utilities";
+import SoundHoverElement from "@/components/ui-abc/sound-hover-element";
+import { SoundTypeElement } from "@/types/sound";
+import { Button } from "@/components/ui/button";
+import { GripVertical } from "lucide-react";
+import { ItemTask, Priority } from "@/types/drag-and-drop.model";
+import { TaskItem } from "./task-item";
+
+export type RenderItemProps = {
+  dragOverlay: boolean;
+  dragging: boolean;
+  sorting: boolean;
+  index: number | undefined;
+  fadeIn: boolean;
+  listeners: DraggableSyntheticListeners;
+  ref: React.Ref<HTMLElement>;
+  style: React.CSSProperties | undefined;
+  transform: Props["transform"];
+  transition: Props["transition"];
+  value: Props["value"];
+  task: ItemTask;
+};
+
+export interface Props {
+  dragOverlay?: boolean;
+  color?: string;
+  disabled?: boolean;
+  dragging?: boolean;
+  handle?: boolean;
+  handleProps?: React.HTMLAttributes<HTMLButtonElement> & {
+    ref?: React.Ref<HTMLButtonElement>;
+  };
+  height?: number;
+  index?: number;
+  fadeIn?: boolean;
+  transform?: Transform | null;
+  listeners?: DraggableSyntheticListeners;
+  sorting?: boolean;
+  style?: React.CSSProperties;
+  transition?: string | null;
+  value: React.ReactNode;
+  task: ItemTask;
+  onRemove?(): void;
+  renderItem?: (args: RenderItemProps) => React.ReactElement;
+  onToggle?: (id: UniqueIdentifier, value: boolean) => void;
+  onChangeTask?: (
+    id: UniqueIdentifier,
+    title: string,
+    priority: Priority,
+    time: number,
+    wastedTime: number
+  ) => void;
+}
+
+export const Item = React.memo(
+  React.forwardRef<HTMLLIElement, Props>(
+    (
+      {
+        dragOverlay,
+        dragging,
+        fadeIn,
+        handle,
+        index,
+        listeners,
+        onChangeTask,
+        renderItem,
+        sorting,
+        style,
+        transition,
+        transform,
+        value,
+        task,
+        onToggle,
+        ...props
+      },
+      ref
+    ) => {
+      useEffect(() => {
+        console.log("Setting cursor to grabbing", dragOverlay);
+        if (!dragOverlay) {
+          return;
+        }
+        document.body.style.cursor = "grabbing";
+
+        return () => {
+          document.body.style.cursor = "";
+        };
+      }, [dragOverlay]);
+
+      return renderItem ? (
+        renderItem({
+          dragOverlay: Boolean(dragOverlay),
+          dragging: Boolean(dragging),
+          sorting: Boolean(sorting),
+          index,
+          fadeIn: Boolean(fadeIn),
+          listeners,
+          ref,
+          style,
+          transform,
+          transition,
+          value,
+          task,
+        })
+      ) : (
+        <li className="list-none" ref={ref} tabIndex={!handle ? 0 : undefined}>
+          <TaskItem
+            index={index}
+            dragging={dragOverlay}
+            task={task}
+            onToggle={onToggle}
+            onChangeTask={(id, title, priority, time, timeDone) => {
+              if (onChangeTask) {
+                onChangeTask(id, title, priority, time, timeDone);
+              }
+            }}
+          >
+            {
+              <div>
+                <SoundHoverElement
+                  animValue={0.9}
+                  hoverTypeElement={SoundTypeElement.SHIFT}
+                >
+                  <Button
+                    data-cypress="draggable-item"
+                    {...(!handle ? listeners : undefined)}
+                    {...props}
+                    variant="ghost"
+                    size="icon"
+                    className="cursor-move hover:bg-background hover:text-foreground"
+                  >
+                    <GripVertical />
+                  </Button>
+                </SoundHoverElement>
+              </div>
+            }
+          </TaskItem>
+        </li>
+      );
+    }
+  )
+);
