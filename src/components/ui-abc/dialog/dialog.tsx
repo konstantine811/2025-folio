752d44aaf1593fe299f874484bc53d7724ca9c75
konstantine811 <galystyan777@gmail.com>
2025-06-06 22:56:43 +0300

    refactor: remove unused AnimatePresence import in DialogTask component

--- src/components/ui-abc/dialog/dialog.tsx
+++ src/components/ui-abc/dialog/dialog.tsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useHoverStore } from "@/storage/hoverStore";
 import { HoverStyleElement } from "@/types/sound";
 import { useEffect } from "react";
