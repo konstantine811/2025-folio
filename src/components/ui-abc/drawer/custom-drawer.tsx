a05a6048e446904e629c9476ce4838f61aabe922
konstantine811 <galystyan777@gmail.com>
2025-06-08 02:40:17 +0300

    refactor: comment out unused imports and code in CustomDrawer component

--- src/components/ui-abc/drawer/custom-drawer.tsx
+++ src/components/ui-abc/drawer/custom-drawer.tsx
@@ -1,63 +1,63 @@
-import { Button } from "@/components/ui/button";
-import { useTranslation } from "react-i18next";
-import SoundHoverElement from "../sound-hover-element";
-import { ArrowBigLeft } from "lucide-react";
-import { useState } from "react";
-import { ScrollArea } from "@/components/ui/scroll-area";
+// import { Button } from "@/components/ui/button";
+// import { useTranslation } from "react-i18next";
+// import SoundHoverElement from "../sound-hover-element";
+// import { ArrowBigLeft } from "lucide-react";
+// import { useState } from "react";
+// import { ScrollArea } from "@/components/ui/scroll-area";
 import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
+  // Drawer,
+  // DrawerContent,
+  // DrawerDescription,
   DrawerDirection,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
+  // DrawerHeader,
+  // DrawerTitle,
+  // DrawerTrigger,
 } from "./drawer";
-import { cn } from "@/utils/classname";
-import { useHeaderSizeStore } from "@/storage/headerSizeStore";
+// import { cn } from "@/utils/classname";
+// import { useHeaderSizeStore } from "@/storage/headerSizeStore";
 import SideMenu from "./drawer-2";
 
 const CustomDrawer = ({
-  title,
-  description,
+  // title,
+  // description,
   children,
-  direction = "right",
-}: {
+}: // direction = "right",
+{
   title: string;
   description: string;
   children: React.ReactNode;
   direction?: DrawerDirection;
 }) => {
-  const [open, setOpen] = useState(false);
-  const [t] = useTranslation();
-  const hs = useHeaderSizeStore((s) => s.size);
-  const buttonTriggerDirectionClass = (direction: DrawerDirection) => {
-    switch (direction) {
-      case "right":
-        return "right-0 rounded-r-none";
-      case "top":
-        return "top-[100px] rounded-t-none";
-      case "bottom":
-        return "bottom-[150px] rounded-b-none";
-      case "left":
-        return "left-0 rounded-l-none";
-    }
-  };
+  // const [open, setOpen] = useState(false);
+  // const [t] = useTranslation();
+  // const hs = useHeaderSizeStore((s) => s.size);
+  // const buttonTriggerDirectionClass = (direction: DrawerDirection) => {
+  //   switch (direction) {
+  //     case "right":
+  //       return "right-0 rounded-r-none";
+  //     case "top":
+  //       return "top-[100px] rounded-t-none";
+  //     case "bottom":
+  //       return "bottom-[150px] rounded-b-none";
+  //     case "left":
+  //       return "left-0 rounded-l-none";
+  //   }
+  // };
 
-  const buttonDirectioinIcon = (direction: DrawerDirection) => {
-    switch (direction) {
-      case "right":
-        return <ArrowBigLeft className="rotate-0" />;
-      case "top":
-        return <ArrowBigLeft className="rotate-90" />;
-      case "bottom":
-        return <ArrowBigLeft className="rotate-270" />;
-      case "left":
-        return <ArrowBigLeft className="rotate-180" />;
-      default:
-        return <ArrowBigLeft />;
-    }
-  };
+  // const buttonDirectioinIcon = (direction: DrawerDirection) => {
+  //   switch (direction) {
+  //     case "right":
+  //       return <ArrowBigLeft className="rotate-0" />;
+  //     case "top":
+  //       return <ArrowBigLeft className="rotate-90" />;
+  //     case "bottom":
+  //       return <ArrowBigLeft className="rotate-270" />;
+  //     case "left":
+  //       return <ArrowBigLeft className="rotate-180" />;
+  //     default:
+  //       return <ArrowBigLeft />;
+  //   }
+  // };
 
   return (
     <>
