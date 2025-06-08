a4fe6bf7e91d203fc6063286e1469577eedee5dd
konstantine811 <galystyan777@gmail.com>
2025-06-08 03:06:26 +0300

    fix: update drag elasticity for improved responsiveness in DrawerContent

--- src/components/ui-abc/drawer/drawer.tsx
+++ src/components/ui-abc/drawer/drawer.tsx
@@ -127,7 +127,7 @@ export const DrawerContent = ({
           dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
           dragElastic={{
             left: 0,
-            right: 0.4,
+            right: 1,
           }}
           dragControls={controls}
           onDragEnd={() => {
